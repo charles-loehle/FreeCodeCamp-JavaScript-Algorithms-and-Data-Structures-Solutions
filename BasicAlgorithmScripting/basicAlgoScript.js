aa55f935866f84d0494c10cc188a1ddab07f10e5
Charles Loehle <clguitar1@gmail.com>
2021-07-27 15:27:58 -0400

    another commit

--- BasicAlgorithmScripting/basicAlgoScript.js
+++ BasicAlgorithmScripting/basicAlgoScript.js
@@ -98,3 +98,157 @@ function repeatStringNumTimes(str, num) {
 }
 
 // console.log(repeatStringNumTimes('abc', 3));
+
+/* ============================================= */
+/* Truncate a String */
+function truncateString(str, num) {
+	if (num >= str.length) {
+		return str.slice(0, num);
+	} else {
+		return str.slice(0, num) + '...';
+	}
+}
+
+// console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length));
+
+/* ============================================= */
+/* Finders Keepers */
+function findElement(arr, func) {
+	let num = 0;
+	for (let i = 0; i < arr.length; i++) {
+		// console.log(func(arr[i]))
+		if (func(arr[i]) === true) {
+			return (num = arr[i]);
+		} else {
+			num = undefined;
+		}
+	}
+	return num;
+}
+
+// console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }))
+
+/* =========================== */
+/* Title Case a Sentence */
+function titleCase(str) {
+	const strToArray = str.toLowerCase().split(' ');
+	let newArr = [];
+	for (let i = 0; i < strToArray.length; i++) {
+		//console.log(strToArray[i])
+		newArr.push(strToArray[i][0].toUpperCase() + strToArray[i].substring(1));
+	}
+	return newArr.join(' ');
+}
+
+// console.log(titleCase("I'm a little tea pot"));
+
+/* ========================================= */
+/* Slice and Splice UNFINISHED */
+function frankenSplice(arr1, arr2, n) {
+	return arr2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+/* ========================================= */
+/* Falsey Bouncer */
+
+function bouncer(arr) {
+	const mapped = arr.filter(item => {
+		if (
+			item !== '' ||
+			item !== false ||
+			item !== NaN ||
+			item !== null ||
+			item !== 0
+		)
+			return item;
+	});
+
+	return mapped;
+}
+
+console.log(bouncer([7, 'ate', '', false, 9, NaN]));
+// false, null, 0, "", undefined, and NaN
+
+function bouncer2(arr) {
+	let newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		console.log(typeof arr[i]);
+		// if statement checks for falsiness or truthiness
+		if (arr[i]) {
+			console.log(arr[i]);
+			newArr.push(arr[i]);
+		}
+	}
+
+	return newArr;
+}
+
+// console.log(bouncer2([7, "ate", "", false, 9, NaN, 0]));
+
+/* ========================================= */
+/* Where Do I Belong */
+function getIndexToIns(arr, num) {
+	arr.push(num);
+	let newArr = [];
+	const sorted = arr.sort((a, b) => a - b);
+	for (let i = 0; i < sorted.length; i++) {
+		// console.log(sorted[i] === num)
+		if (sorted[i] === num) {
+			newArr.push(i);
+		}
+	}
+	return newArr[0];
+}
+
+// console.log(getIndexToIns([10, 20, 30, 40, 50], 35));
+
+/* ========================================= */
+/* Mutations */
+// https://stackoverflow.com/questions/1885557/simplest-code-for-array-intersection-in-javascript
+function mutation(arr) {
+	const array1 = arr[0].toLowerCase().split('');
+	const array2 = arr[1].toLowerCase().split('');
+	// console.log(array2)
+	const mappedArray = array2.map(value => array1.includes(value));
+	// console.log(mappedArray);
+	// console.log(mappedArray.includes(false))
+
+	if (mappedArray.includes(false)) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+// console.log(mutation(["Alien", "line"]));
+
+/* ========================================= */
+/* Chunky Monkey */
+function chunkArrayInGroups(arr, size) {
+	let tempArray = [];
+	for (let i = 0; i < arr.length; i += size) {
+		// console.log(arr.slice(i, i + size))
+		// console.log(tempArray);
+		tempArray.push(arr.slice(i, i + size));
+	}
+	// console.log(tempArray);
+	return tempArray;
+}
+
+console.log(chunkArrayInGroups(['a', 'b', 'c', 'd'], 2));
+
+// program to split array into smaller chunks
+// https://www.programiz.com/javascript/examples/split-array
+function splitIntoChunk(arr, chunk) {
+	for (i = 0; i < arr.length; i += chunk) {
+		let tempArray;
+		tempArray = arr.slice(i, i + chunk);
+		console.log(tempArray);
+	}
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+const chunk = 2;
+splitIntoChunk(array, chunk);
--- BasicJS/main.js
+++ BasicJS/main.js
@@ -189,19 +189,19 @@ function updateRecords(records, id, prop, value) {
 /* Nesting For Loops ======================================================== */
 
 // Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
-// function multiplyAll(arr) {
-//   var product = 1;
-//   // Only change code below this line
-//    for(let i = 0; i < arr.length; i++){
-//      for(let j = 0; j < arr[i].length; j++){
-//        product *= arr[i][j]
-//      }
-//    }
-//   // Only change code above this line
-//   return product;
-// }
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+   for(let i = 0; i < arr.length; i++){
+     for(let j = 0; j < arr[i].length; j++){
+       product *= arr[i][j]
+     }
+   }
+  // Only change code above this line
+  return product;
+}
 
-// console.log(multiplyAll([[5,1],[0.2, 4, 0.5],[3, 9]]));
+console.log(multiplyAll([[5,1],[0.2, 4, 0.5],[3, 9]]));
 
 /* ================================================================ */
 /* Generate Random Whole Numbers within a Range ==================================== */
